0ee95f69d35af59ee9b5aa5d488a175c8c05d148
Fareed Khan <127206523+Fareedkhan56@users.noreply.github.com>
2023-10-07 17:42:34 +0500

    Add files via upload

---
+++ polymorphism/Animals/Animals/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+  /*
+   * overLoading polymorphism
+   internal class Program
+    {
+        void Dog(string dogName,string dogFood)
+        {
+            Console.WriteLine("your dog name is "+dogName);
+            Console.WriteLine("your dogs food is "+dogFood);
+        }
+        void Dog(string dogColor, int dogHouse)
+        {
+            Console.WriteLine("you dog color is "+dogColor);
+            Console.WriteLine("your dog address is "+dogHouse);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Dog("tommy" , "fish");
+            obj.Dog("black", 101);
+            Console.ReadLine();
+        }
+    } */
+
+  /*
+   * overriding polymorphism
+   * class add
+    {
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("class add method is working"+a + b);
+        }
+    }
+    class calculator:add
+    {
+        void sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+       public static void Main(string[] args)
+        {
+           calculator obj = new calculator();
+            obj.sum(20, 30);
+            Console.ReadLine();
+        }
+    }*/
+}
